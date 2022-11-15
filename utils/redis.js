f52dc056f6c5ce0e20857cdd413ed28c1fe62c3a
3599josephw <3599@holbertonschool.com>
2022-11-15 12:33:16 -0600

    redis

---
+++ utils/redis.js
@@ -0,0 +1,18 @@
+/* RedisClient */
+import { createClient } from 'redis';
+class RedisClient {
+
+  constructor() {
+    this.client = createClient();
+    this.client.on('connect', () => this.isAlive(true));
+    //this.client.on('error', (err) => this.isAlive(false), console.log(err));
+  }
+
+  isAlive() {
+    const response = this.client.ping();
+    console.log('isALive:' + response);
+  }
+}
+
+const redisClient = new RedisClient
+module.exports = redisClient;
