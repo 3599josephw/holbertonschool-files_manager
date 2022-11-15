3aa36adeb01c03605559d0f3be5a09b61d6e9632
3599josephw <3599@holbertonschool.com>
2022-11-15 12:39:53 -0600

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -4,13 +4,14 @@ class RedisClient {
 
   constructor() {
     this.client = createClient();
-    this.client.on('connect', () => this.isAlive(true));
+    //this.client.on('connect', () => this.isAlive(true));
     //this.client.on('error', (err) => this.isAlive(false), console.log(err));
   }
 
   isAlive() {
     const response = this.client.ping();
     console.log('isALive:' + response);
+    return true;
   }
 }
 
