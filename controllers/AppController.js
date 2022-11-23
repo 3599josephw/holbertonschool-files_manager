d8537c310e98d2d2a9f6ad47f494e5835f1df224
3599josephw <3599@holbertonschool.com>
2022-11-23 11:37:11 -0600

    removed return statement

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,13 +6,13 @@ class AppController {
   static getStatus(req, resp) {
     const redisAlive = reds.isAlive();
     const mongoAlive = db.isAlive();
-    return resp.status(200).json({ redis: redisAlive, db: mongoAlive });
+    resp.status(200).json({ redis: redisAlive, db: mongoAlive });
   }
 
   static async getStats(req, resp) {
     const userNum = await db.nbUsers();
     const fileNum = await db.nbFiles();
-    return resp.status(200).json({ users: userNum, files: fileNum });
+    resp.status(200).json({ users: userNum, files: fileNum });
   }
 }
 
